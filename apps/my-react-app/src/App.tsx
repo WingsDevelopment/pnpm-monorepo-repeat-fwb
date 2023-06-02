35794a41bd177038a370e9373ebcb72084f904b3
Srdjan Rakic <srdjanr.1609@gmail.com>
2023-06-02 08:49:49 +0200

    added form provider

--- apps/my-react-app/src/App.tsx
+++ apps/my-react-app/src/App.tsx
@@ -1,51 +1,29 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Button, SharedTextField } from "shared-ui";
 import { TextField } from "@mui/material";
-import { useForm } from "react-hook-form";
-
-// create context
-
-const MyContext = React.createContext({
-  name: "test",
-  age: 10,
-});
+import { FormProvider, useForm } from "react-hook-form";
 
 function App() {
+  const methods = useForm();
   const {
     register,
-    handleSubmit,
-    watch,
     formState: { errors },
-  } = useForm();
+    handleSubmit,
+  } = methods;
   const onSubmit = (data: any) => console.log(data);
 
-  const context = React.useContext(MyContext);
   return (
     <div className="App">
-      <MyContext.Provider value={{ name: "test", age: 10 }}>
-        <form
-          style={{
-            border: "1px solid red",
-            padding: "10px",
-            display: "flex",
-            flexDirection: "column",
-            textAlign: "left",
-            maxWidth: "300px",
-            margin: "15px",
-            gap: "15px",
-          }}
-          onSubmit={handleSubmit(onSubmit)}
-        >
+      <FormProvider {...methods}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          shared:
+          <SharedTextField name="test" value={"test"} />
           form
           <TextField />
           <input type="submit" />
         </form>
-        shared:
-        <SharedTextField name="test" value={"test"} />
-        <Button onClick={() => console.log("clicked")}>Click me</Button>
-      </MyContext.Provider>
+      </FormProvider>
+      <Button onClick={() => console.log("clicked")}>Click me</Button>
     </div>
   );
 }
